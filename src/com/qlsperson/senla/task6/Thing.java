84bdc6dcfa08651977cb57943a44c85d35d98aa7
Alexei Ivanov <setlovetobe@gmail.com>
2019-09-24 11:04:20 +0300

    Added exception and changed access

--- src/com/qlsperson/senla/task6/Thing.java
+++ src/com/qlsperson/senla/task6/Thing.java
@@ -1,17 +1,22 @@
 package com.qlsperson.senla.task6;
 
-public class Thing {
+import java.util.InputMismatchException;
+
+class Thing {
     private int number;
     private int weight;
     private int cost;
 
-    public Thing(int number, int weight, int cost) {
+    Thing(int number, int weight, int cost) {
+        if(weight <= 0 || cost <= 0 ) {
+            throw new InputMismatchException("Вес и стоимость должны быть больше 0");
+        }
         this.number = number;
         this.weight = weight;
         this.cost = cost;
     }
 
-    public int getNumber() { return number; }
-    public int getWeight() { return weight; }
-    public int getCost() { return cost; }
+    int getNumber() { return number; }
+    int getWeight() { return weight; }
+    int getCost() { return cost; }
 }

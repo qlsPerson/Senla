e6e21ed0efa56b2fd3a912542a4700e7ba8930a2
Alexei Ivanov <setlovetobe@gmail.com>
2019-09-26 16:28:34 +0300

    Added Cargo and fixed Java Code Conventions

--- src/com/qlsperson/senla/task6/Main.java
+++ src/com/qlsperson/senla/task6/Main.java
@@ -6,13 +6,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        try(Scanner scanner = new Scanner(System.in)) {
+        try (Scanner scanner = new Scanner(System.in)) {
             ArrayList<Thing> things = new ArrayList<>();
             System.out.print("Введите грузоподъемность рюкзака: ");
             Backpack backpack = new Backpack(scanner.nextInt());
             System.out.print("Введите количество вещей: ");
             int countThings = scanner.nextInt();
-            if(countThings > 0) {
+            if (countThings > 0) {
                 for (int i = 1; i <= countThings; i++) {
                     System.out.print("Введите вес вещи №" + i + ": ");
                     int weightThing = scanner.nextInt();
@@ -21,18 +21,18 @@ public class Main {
                     things.add(new Thing(i, weightThing, costThing));
                 }
                 backpack.fillBackpack(things);
-                for (Thing t :backpack.getBestThings()) {
+                for (Thing t : backpack.getThings()) {
                     System.out.print("Вещь под №" + t.getNumber());
                     System.out.print(" c весом: " + t.getWeight());
                     System.out.print(" и стоимостью: " + t.getCost());
                     System.out.println(" попала в рюкзак.");
                 }
-                System.out.println("Вес рюкзака с вещами: " + backpack.getCapacity());
-                System.out.println("Суммарная стоимость рюкзака: " + backpack.getBestCost());
+                System.out.println("Вес груза в рюкзаке: " + backpack.getWeightThings());
+                System.out.println("Суммарная ценность груза в рюкзаке: " + backpack.getCostThings());
             } else {
                 throw new InputMismatchException("Количество вещей должно быть больше 0");
             }
-        }catch (InputMismatchException ex) {
+        } catch (InputMismatchException ex) {
             ex.printStackTrace();
         }
     }
